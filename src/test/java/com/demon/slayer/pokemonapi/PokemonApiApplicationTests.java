5f9aff1d9159a8ccc99b97c26ff40532b86265d5
Martin Guajardo <e-jmguajardo@neoris.com>
2022-02-21 09:33:41 -0600

    init commit

---
+++ src/test/java/com/demon/slayer/pokemonapi/PokemonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demon.slayer.pokemonapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
