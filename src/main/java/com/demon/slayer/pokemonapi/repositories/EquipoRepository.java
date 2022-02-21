f3555912255a9084e1712e8ffd647b047d83d0d7
RubiGpe <e-rgosuna@neoris.com>
2022-02-21 11:05:08 -0700

    Agregando Repositorios

---
+++ src/main/java/com/demon/slayer/pokemonapi/repositories/EquipoRepository.java
@@ -0,0 +1,5 @@
+package com.demon.slayer.pokemonapi.repositories;
+
+public interface EquipoRepository extends JpaRepository<Usuario, String> {
+    
+}
---
+++ src/main/java/com/demon/slayer/pokemonapi/repositories/TipoRepository.java
@@ -0,0 +1,7 @@
+package com.demon.slayer.pokemonapi.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TipoRepository extends JpaRepository<Usuario, String>  {
+    
+}
